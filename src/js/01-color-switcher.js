382854ebb313c021c1a9540522233c489f1e348a
Katharinaad <vermilion.adams@gmail.com>
2023-08-08 14:33:36 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const onBtn = document.querySelector(['button[data-start']);
+const offBtn = document.querySelector(['button[data-stop']);
 
+let intervalId = null;
+offBtn.disabled = true;
+
+onBtn.addEventListener('click', () => {
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  onBtn.disabled = true;
+  offBtn.disabled = false;
+});
+
+offBtn.addEventListener('click', () => {
+  clearInterval(intervalId);
+  offBtn.disabled = true;
+  onBtn.disabled = false;
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
